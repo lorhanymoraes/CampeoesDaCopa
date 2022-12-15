ea6c3bf4f1e64c6a9e9fac9efc8d7486669afcb9
lorhanymoraes <107012585+lorhanymoraes@users.noreply.github.com>
2022-12-15 16:11:18 -0300

    project

---
+++ CampeoesDaCopa/Controllers/WorldCupTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  WorldCupTableViewCell.swift
+//  CampeoesDaCopa
+//
+//  Created by Lorhany Moraes on 01/06/22.
+//
+
+import UIKit
+
+class WorldCupTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbYear: UILabel!
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lvVice: UILabel!
+    @IBOutlet weak var lbWinnerScore: UILabel!
+    @IBOutlet weak var lbViceScore: UILabel!
+    @IBOutlet weak var lbCountry: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func prepare(with cup: WorldCup) {
+        lbYear.text = "\(cup.year)"
+        ivWinner.image = UIImage(named: cup.winner)
+        ivVice.image = UIImage(named: cup.vice)
+        lbWinner.text = cup.winner
+        lvVice.text = cup.vice
+        lbCountry.text = cup.country
+        lbWinnerScore.text = cup.winnerScore
+        lbViceScore.text = cup.viceScore
+        
+    }
+
+}
